44eb909f16e7cb6bfbfe5443251722edd00f3e83
river <yybffds@126.com>
2021-01-18 11:30:09 +0800

    feign 抽离专门的模块

--- platform-server/user-server/user-server-api/src/main/java/com/river/platform/user/client/UserClient.java
+++ platform-server/user-server/user-server-api/src/main/java/com/river/platform/user/client/UserClient.java
@@ -1,4 +1,4 @@
-package com.river.platform.service;
+package com.river.platform.user.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
